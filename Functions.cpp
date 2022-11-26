ef13c8f938439b1632d1612cbfd6e22469b6a5b9
MaxPunish <118986293+MaxPunish@users.noreply.github.com>
2022-11-26 16:05:15 +0200

    Add files via upload

---
+++ Functions.cpp
@@ -0,0 +1,8 @@
+
+#include "Functions.h"
+int minimumas(int a, int b) {
+    return (a > b) ? b : a;
+}
+int maximumas(int a, int b) {
+    return (a < b) ? b : a;
+}
---
+++ Functions.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int minimumas(int, int);
+int maximumas(int, int);
